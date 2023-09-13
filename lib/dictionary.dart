ea28db26b9a569d198f5ed9cc95e254e0295a5aa
ramdhanyA <ariqathallah38@gmail.com>
2023-09-13 09:01:55 +0900

    Entries and Entry classes

--- lib/dictionary.dart
+++ lib/dictionary.dart
@@ -2,21 +2,13 @@ import 'package:jm_dict_en/entry.dart';
 import 'package:xml/xml.dart';
 
 class Dictionary {
-  List<Entry> entries = [];
+  Entries entries = [];
 
   void loadFromXml({String xmlString = "assets/JMdict_e.xml"}) {
     try {
       final document = XmlDocument.parse(xmlString);
       int i = 0;
-      for (XmlNode node in document.children) {
-        if (i >= 10) break;
-        for (XmlNode nodeChild in node.children) {
-          if (nodeChild is XmlElement) {
-            print(nodeChild.name);
-            i++;
-          }
-        }
-      }
+      entries = Entries(document.children.toList());
     } catch (e) {
       print('Failed to parse XML: $e');
     }
--- lib/entry.dart
+++ lib/entry.dart
@@ -1,7 +1,30 @@
-class Entry {
+import 'package:xml/xml.dart';
+
+class Entries {
+  final List<Entry> entries;
+  Entries(this.entries);
+}
+
+class Entry extends XmlNode {
+  final XmlElement element;
   final String word;
   final String definition;
   final String partOfSpeech;
 
-  Entry(this.word, this.definition, this.partOfSpeech);
+  Entry(this.word, this.definition, this.partOfSpeech)
+      : element = XmlElement(XmlName('entry')),
+        super();
+
+  @override
+  XmlNode copy() {
+    return Entry(word, definition, partOfSpeech);
+  }
+
+  @override
+  void accept(XmlVisitor visitor) {
+    visitor.visitElement(element);
+  }
+
+  @override
+  XmlNodeType get nodeType => XmlNodeType.ELEMENT;
 }
